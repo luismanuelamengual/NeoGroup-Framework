cac3a60fb0fe83c4dafbf3b9cb17d3a9191150bb
Luis <luis.amengual@sitrack.com>
2017-02-06 17:15:11 -0300

    Added default name for WebAction

--- src/org/neogroup/sparks/web/processors/WebAction.java
+++ src/org/neogroup/sparks/web/processors/WebAction.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 public @interface WebAction {
 
-    public String name();
+    public String name() default "";
 }
--- test/org/neogroup/sparks/processors/TestProcessor.java
+++ test/org/neogroup/sparks/processors/TestProcessor.java
@@ -10,6 +10,13 @@ import org.neogroup.sparks.web.processors.WebRoute;
 @WebRoute(path="/test/")
 public class TestProcessor extends WebProcessor {
 
+    @WebAction
+    public HttpResponse indexAction (HttpRequest request) {
+        HttpResponse response = new HttpResponse();
+        response.setBody("Hi, this is the index action !!");
+        return response;
+    }
+
     @WebAction(name="pepe")
     public HttpResponse pepeAction (HttpRequest request) {
         HttpResponse response = new HttpResponse();
