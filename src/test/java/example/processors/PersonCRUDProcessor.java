a46b3f10bb0189fe72135cf3a10e7e21faf651d0
luis <luismanuelamengual@gmail.com>
2017-05-13 10:45:27 -0300

    Changes for maven deploy

--- src/test/java/example/processors/PersonCRUDProcessor.java
+++
@@ -1,8 +0,0 @@
-package example.processors;
-
-import example.models.Person;
-import org.neogroup.sparks.processors.crud.DataSourceCRUDProcessor;
-
-public class PersonCRUDProcessor extends DataSourceCRUDProcessor<Person> {
-
-}
